065e140d2b7a7ca09400edd58f4bd6a8fd8f15d9
Bernhard Bablok <bablokb@gmx.de>
2020-12-23 19:32:48 +0100

    initial commit

---
+++ projects/include/picconfig_default.h
@@ -0,0 +1,15 @@
+// --------------------------------------------------------------------------
+// pic-toolchain: default macros
+//
+// Note: this file is included from the files picconfig_*.h
+//
+// Author: Bernhard Bablok
+//
+// https://github.com/bablokb/pic-toolchain
+//
+// --------------------------------------------------------------------------
+
+#ifndef PICCONFIG_DEF_H
+  #define PICCONFIG_DEF_H
+
+#endif
