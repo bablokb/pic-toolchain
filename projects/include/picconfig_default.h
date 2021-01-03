4bf0265175c36944740d784be23399245c8fb6f4
Bernhard Bablok <bablokb@gmx.de>
2021-01-03 07:32:20 +0100

    added missing macros for xc8

--- projects/include/picconfig_default.h
+++ projects/include/picconfig_default.h
@@ -15,4 +15,15 @@
   #define bitset(var, bitno)   ((var) |= 1UL << (bitno))
   #define bitread(var, bitno)  ((var) & (1UL << (bitno)))
   #define bitclear(var, bitno) ((var) &= ~(1UL << (bitno)))
+
+  #ifdef __XC8
+    #ifndef _CONCAT
+      #undef  _CONCAT2
+      #define _CONCAT(a,b)  _CONCAT2(a,b)
+      #define _CONCAT2(a,b) a ## b
+    #endif
+    #ifndef __asm__
+      #define __asm__(code) asm(code)
+    #endif
+  #endif
 #endif
