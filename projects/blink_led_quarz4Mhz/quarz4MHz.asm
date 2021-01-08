3c989a41705f9b2ada251699c6b02849d65cb73e
Bernhard Bablok <bablokb@gmx.de>
2021-01-08 15:46:17 +0100

    added : to labels

--- projects/blink_led_quarz4Mhz/quarz4MHz.asm
+++ projects/blink_led_quarz4Mhz/quarz4MHz.asm
@@ -18,25 +18,25 @@ time2           res 1
 ;"x" (dezimal) muß als Parameter in W vorher geladen worden sein
 ;#################################################################
         code
-_minitime
+_minitime:
 	movwf	miniteil		;"x"=W=miniteil
-mm0	call	time100		;100µs Verzögerung
+mm0:    call	time100		;100µs Verzögerung
 	decf	miniteil,F		;miniteil=miniteil-1
 	btfss	STATUS,Z	;wenn Z=1, überspringe nä. Bef.
 	goto	mm0		;Z=0, miniteil>0
 	return			;Z=1, miniteil=0, Verzögerung erreicht
 ;
-_miditime
+_miditime:
 	movwf	miditeil		;"x"=W=miditeil
-mm1	call	time1ms		;1ms Verzögerung
+mm1:    call	time1ms		;1ms Verzögerung
 	decf	miditeil,F		;miditeil=miditeil-1
 	btfss	STATUS,Z	;wenn Z=1, überspringe nä. Bef.
 	goto	mm1		;Z=0, miditeil>0
 	return			;Z=1, miditeil=0, Verzögerung erreicht
 ;
-_maxitime
+_maxitime:
 	movwf	maxiteil		;"x"=W=maxiteil
-mm2	call	time250ms	;250ms Verzögerung
+mm2:    call	time250ms	;250ms Verzögerung
 	decf	maxiteil,F		;maxiteil=maxiteil-1
 	btfss	STATUS,Z	;wenn Z=1, überspringe nä. Bef.
 	goto	mm2		;Z=0, maxiteil>0
@@ -45,10 +45,10 @@ mm2	call	time250ms	;250ms Verzögerung
 ;#################################################################
 ;UP time100: Zeitschleife 100 µs (23x4=92+8=100 Zyklen a 1 µs)
 ;#################################################################
-time100
+time100:
 	movlw	0x17		;W =17 hex , =23 dez
 	movwf	time0		;w->time0
-m0	decf	time0,F		;time0 decrem., wenn 0, Z=1
+m0:     decf	time0,F		;time0 decrem., wenn 0, Z=1
 	btfss	STATUS,Z	;wenn Z=1, überspringe nä. Bef.
 	goto	m0		;Z=0, weil time0 > 0, springe zu m0
 	nop			;Z=1, weil time0 = 0
@@ -58,10 +58,10 @@ m0	decf	time0,F		;time0 decrem., wenn 0, Z=1
 ;#################################################################
 ;UP time1ms: Zeitschleife 1 ms (248x4=992+8=1000 Zyklen a 1µs) 
 ;#################################################################
-time1ms
+time1ms:
 	movlw	0xf8		;W =F8 hex , =248 dez
 	movwf	time1		;w->time1
-m1	decf	time1,F		;time1 decrem., wenn 0, Z=1
+m1:     decf	time1,F		;time1 decrem., wenn 0, Z=1
 	btfss	STATUS,Z	;
 	goto	m1		;
 	nop
@@ -71,10 +71,10 @@ m1	decf	time1,F		;time1 decrem., wenn 0, Z=1
 ;#################################################################
 ;UP time250ms: Zeitschleife 250 ms (250x1ms=250ms+6µs)
 ;#################################################################
-time250ms
+time250ms:
 	movlw	0xfa		;W =FA hex , =250 dez
 	movwf	time2		;w->time2
-m2	call	time1ms		;Zeitverzögerung 1ms
+m2:     call	time1ms		;Zeitverzögerung 1ms
 	decf	time2,F		;time2 decrem., wenn 0, Z=1
 	btfss	STATUS,Z	;wenn Z=1, überspringe nä. Bef.
 	goto	m2
