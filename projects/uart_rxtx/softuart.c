0ad151404b384090e8284c2deb4eda8ec3c9e36f
Bernhard Bablok <bablokb@gmx.de>
2021-01-12 18:51:09 +0100

    migrate to standard

--- projects/uart_rxtx/softuart.c
+++ projects/uart_rxtx/softuart.c
@@ -18,31 +18,30 @@
 // --------------------------------------------------------------------------
 
 void softuart_init(void)	{
-  UART_TX     = 1;     // TX pin is high in idle state
-  UART_RX_DIR = 1;     // Input
-  UART_TX_DIR = 0;     // Output
+  bitset(TRISIO,PIN_UART_RX);      // input
+  bitclear(TRISIO,PIN_UART_TX);    // output
+  GP_TX = 1;                       // TX pin is high in idle state
 }
 
 // --------------------------------------------------------------------------
-
 unsigned char softuart_receive(void) {
   unsigned char DataValue = 0;
 
   //wait for start bit
-  while(UART_RX==1);
+  while(GP_RX==1);
 
   delay_bit();                 // one bit delay
   delay_bit_half();            // Take sample value in the mid of bit duration
 
   for ( unsigned char i = 0; i < DataBitCount; i++ ) {
-    if ( UART_RX == 1 ) {  //if received bit is high
+    if ( GP_RX == 1 ) {  //if received bit is high
       DataValue += (1<<i);
     }
     delay_bit();
   }
 
   // Check for stop bit
-  if ( UART_RX == 1 ) {       //Stop bit should be high
+  if ( GP_RX == 1 ) {       //Stop bit should be high
     delay_bit_half();
     return DataValue;
   } else {                    //some error occurred !
@@ -63,22 +62,22 @@ void softuart_transmit(const unsigned char DataValue) {
   */
 
   // Send Start Bit
-  UART_TX = 0;
+  GP_TX = 0;
   delay_bit();
 
   for ( unsigned char i = 0; i < DataBitCount; i++ ) {
     //Set Data pin according to the DataValue
     if( ((DataValue>>i)&0x1) == 0x1 ) {   //if Bit is high
-      UART_TX = 1;
+      GP_TX = 1;
     } else {     //if Bit is low
-      UART_TX = 0;
+      GP_TX = 0;
     }
 
     delay_bit();
   }
 
   //Send Stop Bit
-  UART_TX = 1;
+  GP_TX = 1;
   delay_bit();
 }
 
